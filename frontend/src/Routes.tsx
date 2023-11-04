3be413159119616383a2a5ec7cbd43553cfea9f5
NoePigeau <npigeau@myges.fr>
2023-11-04 15:59:38 +0100

    design: sidebar admin

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,16 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// ...
-import ClientLayout from "./layouts/Client";
-import Landing from "./pages/Landing";
-import NotFound from "./pages/NotFound";
-import AdminLayout from "./layouts/Admin";
-import HomeAdmin from "./pages/admin/HomeAdmin";
-import HomeProvider from "./pages/provider/HomeProvider";
-import Employees from "./pages/provider/Employees";
+
+import AdminLayout from "@/layouts/admin";
+import ClientLayout from "@/layouts/client";
+
+import Landing from "@/pages/Landing";
+import NotFound from "@/pages/NotFound";
+import HomeAdmin from "@/pages/admin/HomeAdmin";
+import HomeProvider from "@/pages/provider/HomeProvider";
+import Employees from "@/pages/provider/Employees";
 import Login from "@/pages/auth/Login.tsx";
 import Register from "@/pages/auth/Register.tsx";
 import Terms from "@/pages/Terms.tsx";
-import { CompanyPage } from "./pages/client/Company";
+import CompanyPage from "@/pages/client/Company";
 
 const Routes = () => {
   const router = createBrowserRouter([
